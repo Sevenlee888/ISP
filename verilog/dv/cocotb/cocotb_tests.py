2b394352cb96795420100f5a2615036d88c481cb
Chen-Cheng Pin <110382354+Benisonpin@users.noreply.github.com>
2023-09-04 11:52:23 +0800

    Initialize update_documentation

---
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2023 Efabless Corporation
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#      http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from hello_world.hello_world import hello_world
+from hello_world_uart.hello_world_uart import hello_world_uart
+from user_proj_tests.counter_wb.counter_wb import counter_wb
+from user_proj_tests.counter_la.counter_la import counter_la
+from user_proj_tests.counter_la_reset.counter_la_reset import counter_la_reset
+from user_proj_tests.counter_la_clk.counter_la_clk import counter_la_clk
+from gpio_test.gpio_test import gpio_test
